b47a1dd485e07be9a5058f2770770fe199eab19f
OnAir Monitor <alexcande.pro@gmail.com>
2026-06-04 14:47:18 +0700

    chore: bump version to 0.8.0 (FBO)

--- lib/core/app_info.dart
+++ lib/core/app_info.dart
@@ -1,3 +1,3 @@
 ﻿/// App version shown in the UI (About page, drawer header).
 /// Keep in sync with `version:` in pubspec.yaml.
-const String kAppVersion = '0.7.0';
+const String kAppVersion = '0.8.0';
